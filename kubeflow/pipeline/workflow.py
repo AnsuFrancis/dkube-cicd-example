1a475ef0a83787ccafb70cbbfc3317d05f5ec9e9
Ansu Francis <36500728+AnsuFrancis@users.noreply.github.com>
2023-09-20 18:03:15 +0530

    Update workflow.py

--- kubeflow/pipeline/workflow.py
+++ kubeflow/pipeline/workflow.py
@@ -83,8 +83,8 @@ setup_op = components.load_component(text = setup_component)
 def d3pipeline(
     user,
     auth_token,
-    project_id,
     tags,
+    project_id=project_id,
     #Clinical preprocess
     clinical_preprocess_script="python clinical_reg/cli-pre-processing.py",
     clinical_preprocess_datasets=json.dumps(["clinical"]),
