3ed2bf57749418ff24aa535a524fa8053f5a0aa0
Ansu Francis <36500728+AnsuFrancis@users.noreply.github.com>
2023-09-21 15:30:02 +0530

    Update workflow.py

--- kubeflow/pipeline/workflow.py
+++ kubeflow/pipeline/workflow.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Pipeline workflow definition."""
-import sys
-sys.path.insert(0, "/root/miniconda3/envs/dkube-env/lib/python3.7/site-packages")
+#import sys
+#sys.path.insert(0, "/root/miniconda3/envs/dkube-env/lib/python3.7/site-packages")
 
 import kfp.dsl as dsl
 from kfp import components
